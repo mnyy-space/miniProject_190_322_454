91255dd50b3392fa33ec5163fd4f1e7e6cc8e891
mnyy-space <murnee.studentmls@gmail.com>
2026-07-15 15:28:48 +0700

    skilljaa

---
+++ lib/models/skills_model.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+class SkillsModel {
+  int skillId = 0;
+  String skillName = "";
+
+  SkillsModel({
+    required this.skillId,
+    required this.skillName,
+  });
+
+  factory SkillsModel.fromJson(Map<String, dynamic> json) {
+    return SkillsModel(
+      skillId: json['skill_id'] as int,
+      skillName: json['skill_name'] as String
+      );
+  }
+}
+
+class SkillsResponse {
+  bool isError = false;
+  List<SkillsModel> data = [];
+  String errorMessage = "";
+
+  SkillsResponse({
+    required this.isError,
+    required this.data,
+    required this.errorMessage,
+  });
+
+  factory SkillsResponse.fromJson(Map<String, dynamic> json) {
+    return SkillsResponse(
+      isError: json['isError'],
+      data: (json['data'] as List)
+        .map((item) => SkillsModel.fromJson(item))
+        .toList(),
+      errorMessage: json['errorMessage'],
+    );
+  }
+}
+
